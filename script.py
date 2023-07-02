65a0c9cc8adec11686e411f2bfd068d5700f441e
anish-santim <107955181+anish-santim@users.noreply.github.com>
2023-07-02 18:57:02 +0530

    Initial Commit

---
+++ script.py
@@ -0,0 +1,29 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+
+source_node = glueContext.create_dynamic_frame.from_catalog(
+    database="<database-name>",
+    table_name="<table-name>"
+)
+
+
+destination_node = glueContext.write_dynamic_frame.from_options(
+    frame=source_node,
+    connection_type="s3",
+    format="json",
+    connection_options={"path": "s3://<destination-bucket-name>/transformed_data.json", "partitionKeys": []}
+)
+
+job.commit()
